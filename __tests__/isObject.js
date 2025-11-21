4e37b9d0894de5f68146a0285f332c1994dce9e1
Jalmari Mieho <jalmari.mieho@tuni.fi>
2025-11-21 17:00:31 +0200

    isObject tests

---
+++ __tests__/isObject.js
@@ -0,0 +1,36 @@
+import isObject from "../src/isObject.js";
+
+
+test('empty curly bracket', () => {
+    expect(isObject({})).toBe(true);
+})
+
+test('array of three numbers', () => {
+    expect(isObject([1, 2, 3])).toBe(true)
+})
+
+test('function keyword', () => {
+    expect(isObject(Function)).toBe(true)
+})
+
+test('a short written function', () => {
+
+    function add_test(arg1, arg2) {
+        return arg1 + arg2;
+    }
+
+    expect(isObject(add_test)).toBe(true)
+
+})
+
+test('null value', () => {
+    expect(isObject(null)).toBe(false)
+})
+
+test('number five', () => {
+    expect(isObject(5)).toBe(false)
+})
+
+test('string of "dog"', () => {
+    expect(isObject("dog")).toBe(false)
+})
